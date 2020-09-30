d2acfb52d2d2488f8a0ea2ce0ea0a74ff88ec057
mariobot <mariobot113@hotmail.com>
2020-09-30 11:57:45 -0500

    Add routing project

---
+++ routing/src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navegarPosts(){
+    this.router.navigate(['/posts']);
+  }
+}
