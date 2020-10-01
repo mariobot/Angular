fe851a8885c62515ae989fd59fa38cd43331383e
mariobot <mariobot113@hotmail.com>
2020-10-01 15:32:23 -0500

    Add forms project

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
